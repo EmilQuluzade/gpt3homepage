db498acc92ef59cac7d64eca99e829e943bdef2f
thedipi <dipiland777@gmail.com>
2023-08-30 18:58:30 +0400

    Did a one more section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Companies from './Components/Companies';
 import Information from './Components/Information';
 import Introduction from './Components/Introduction';
+import Middlepart from './Components/Middlepart';
 import Navbar from './Components/Navbar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Introduction/>
       <Companies/>
       <Information/>
+      <Middlepart/>
     </div>
   );
 }
---
+++ src/Components/Middlepart.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Middlepart = () => {
+  return (
+    <div className="middlepart">
+        <div className="middleleft">
+<h5>The Future is Now and<br/> You Just Need To Realize<br/> It. Step into Future Today<br/> & Make it Happen.</h5>
+<span>Request Early Access to Get Started</span>
+        </div>
+        <div className="middleright">
+<div className="middleblock">
+    <div className="header">
+<div className="line"></div>
+<h4>Improving end<br/> distrusts instantly</h4>
+    </div>
+    <p>From they fine john he give of rich he. They age and<br/> draw mrs like. Improving end distrusts may<br/> instantly was household applauded.</p>
+</div>
+<div className="middleblock">
+    <div className="header">
+<div className="line"></div>
+<h4>Become the<br/> tended active</h4>
+    </div>
+    <p style={{ paddingLeft: 42 }}>Considered sympathize ten uncommonly<br/> occasional assistance sufficient not. Letter of on<br/> become he tended active enable to.</p>
+</div>
+<div className="middleblock">
+    <div className="header">
+<div className="line"></div>
+<h4>Message or am<br/> nothing</h4>
+    </div>
+    <p style={{ paddingLeft: 30 }}>Led ask possible mistress relation elegance eat<br/> likewise debating. By message or am nothing<br/> amongst chiefly address.</p>
+</div>
+<div className="middleblock">
+    <div className="header">
+<div className="line"></div>
+<h4>Really boy law<br/> county</h4>
+    </div>
+    <p style={{ paddingLeft: 38 }}>Really boy law county she unable her sister. Feet<br/> you off its like like six. Among sex are leave law built<br/>  now. In built table in an rapid blush.</p>
+</div>
+        </div>
+    </div>
+  )
+}
+
+export default Middlepart
